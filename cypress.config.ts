28a862b450c03a95d9f5e26c72a27443e7658573
edcaetanoguedes <ednaldocaetanoguedes@gmail.com>
2025-05-27 21:20:55 -0300

    chore(config): remoção da baseUrl da configuração cypress
    O baseUrl fixava uma url, este projeto faz uso de 2 urls (frontend e backend).
    Ao rodar o cypress apresentava erro.

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,22 +5,26 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000",
+    //baseUrl: "http://localhost:3000",
     specPattern: "cypress/e2e/**/**/*.feature",
-    viewportWidth: 1920, viewportHeight: 1080,
-    
-    async setupNodeEvents(on, config) {
-      await addCucumberPreprocessorPlugin(on, config)
+    viewportWidth: 1920,
+    viewportHeight: 1080,
 
-      on("file:preprocessor", createBundler({
-        plugins: [createEsbuildPlugin(config)]
-      }))
-      return config
-    }
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        }),
+      );
+      return config;
+    },
   },
   env: {
     grepFilterSpecs: true,
     BACKEND_ENDPOINT: "http://localhost:4000/api/v1",
-    FRONTEND_URL: "http://localhost:3000"
-  }
+    FRONTEND_URL: "http://localhost:3000",
+  },
 });
